fd5239bc0db1ad45f060efb7cb1622fafaf7e7cd
BhavyaVasamshetti <142156953+BhavyaVasamshetti@users.noreply.github.com>
2023-12-07 18:57:09 +0530

    Update Student.java

--- JavaProject/src/main/java/com/JavaProject/Entity/Student.java
+++ JavaProject/src/main/java/com/JavaProject/Entity/Student.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 @Entity
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studentId;
     private String firstName;
     private String lastName;
