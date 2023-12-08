ef2ba67d6fcb1c739a4c883013cb75568690922d
BhavyaVasamshetti <142156953+BhavyaVasamshetti@users.noreply.github.com>
2023-12-08 17:45:59 +0530

    Update Enrollment.java

--- JavaProject/src/main/java/com/JavaProject/Entity/Enrollment.java
+++ JavaProject/src/main/java/com/JavaProject/Entity/Enrollment.java
@@ -3,11 +3,17 @@ package com.JavaProject.Entity;
 //importing the neccessary packages
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
 
 @Entity
+@Data
 public class Enrollment {
     @Id
     private Long enrollmentId;
@@ -15,62 +21,13 @@ public class Enrollment {
 
     @ManyToOne
     @JoinColumn(name = "studentId") // This assumes "studentId" is the foreign key column in the Enrollment table
+    @JsonIgnoreProperties("enrollments")
+
     private Student student;
 
     @ManyToOne
     @JoinColumn(name = "courseId") // This assumes "courseId" is the foreign key column in the Enrollment table
+    @JsonIgnoreProperties("enrollments")
+
     private Course course;
-
-    // Default constructor
-    public Enrollment() {
-        super();
-    }
-
-    // Parameterized constructor to initialize the attributes
-    public Enrollment(Long enrollmentId, Date enrollmentDate, Student student, Course course) {
-        super();
-        this.enrollmentId = enrollmentId;
-        this.enrollmentDate = enrollmentDate;
-        this.student = student;
-        this.course = course;
-    }
-
-    // Getter and Setter methods for each attribute
-    public Long getEnrollmentId() {
-        return enrollmentId;
-    }
-
-    public void setEnrollmentId(Long enrollmentId) {
-        this.enrollmentId = enrollmentId;
-    }
-
-    public Date getEnrollmentDate() {
-        return enrollmentDate;
-    }
-
-    public void setEnrollmentDate(Date enrollmentDate) {
-        this.enrollmentDate = enrollmentDate;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-
-    @Override
-    public String toString() {
-        return "Enrollment [enrollmentId=" + enrollmentId + ", enrollmentDate=" + enrollmentDate + ", student=" +
-                student + ", course=" + course + "]";
-    }
 }
