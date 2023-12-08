689d99a122ae0ce1fa50f7388cce0856c231af39
BhavyaVasamshetti <142156953+BhavyaVasamshetti@users.noreply.github.com>
2023-12-08 17:46:45 +0530

    Update Student.java

--- JavaProject/src/main/java/com/JavaProject/Entity/Student.java
+++ JavaProject/src/main/java/com/JavaProject/Entity/Student.java
@@ -1,33 +1,26 @@
 package com.JavaProject.Entity;
 
-//importing the neccessary packages
-import java.sql.Date;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+//importing the neccessary packages
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
-
 @Entity
 @Data
-public class Enrollment {
+public class Student {
     @Id
-    private Long enrollmentId;
-    private Date enrollmentDate;
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
 
     @ManyToOne
-    @JoinColumn(name = "studentId") // This assumes "studentId" is the foreign key column in the Enrollment table
-    @JsonIgnoreProperties("enrollments")
-
-    private Student student;
-
-    @ManyToOne
-    @JoinColumn(name = "courseId") // This assumes "courseId" is the foreign key column in the Enrollment table
-    @JsonIgnoreProperties("enrollments")
-
+    @JoinColumn(name = "courseId")
+    @JsonIgnoreProperties("students")
     private Course course;
 }
+
