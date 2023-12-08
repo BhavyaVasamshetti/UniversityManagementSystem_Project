0905f131ed202f35c2636e45e6188e439175d778
BhavyaVasamshetti <142156953+BhavyaVasamshetti@users.noreply.github.com>
2023-12-08 17:47:08 +0530

    Update Course.java

--- JavaProject/src/main/java/com/JavaProject/Entity/Course.java
+++ JavaProject/src/main/java/com/JavaProject/Entity/Course.java
@@ -1,10 +1,17 @@
 package com.JavaProject.Entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 //importing the neccessary packages
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 @Entity
+@Data
 public class Course {
     @Id
     private Long courseId;
@@ -12,68 +19,8 @@ public class Course {
     private String courseCode;
     private int courseFees;
 
-    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "course")
+    @JsonIgnoreProperties("course")
+
     private List<Student> students;
-
-    // Default constructor
-    public Course() {
-        super();
-    }
-
-    // Parameterized constructor to initialize the attributes
-    public Course(Long courseId, String courseName, String courseCode, int courseFees) {
-        super();
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.courseCode = courseCode;
-        this.courseFees = courseFees;
-    }
-
-    // Getter and Setter methods for each attribute
-    public Long getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(Long courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseCode() {
-        return courseCode;
-    }
-
-    public void setCourseCode(String courseCode) {
-        this.courseCode = courseCode;
-    }
-
-    public int getCourseFees() {
-        return courseFees;
-    }
-
-    public void setCourseFees(int courseFees) {
-        this.courseFees = courseFees;
-    }
-
-    // Additional getters and setters for students
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
-
-    @Override
-    public String toString() {
-        return "Course [courseId=" + courseId + ", courseName=" + courseName + ", courseCode=" + courseCode
-                + ", courseFees=" + courseFees + ", students=" + students + "]";
-    }
 }
